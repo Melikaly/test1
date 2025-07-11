bebc8fab0ccf4abdebb636af3f9e3b5a95ec0956
Melika Arab <melikaly19@gmail.com>
2025-07-12 00:59:37 +0330

    change javascript

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
-function myDisplayer(some) {
-    document.getElementById("demo").innerHTML = some;
-}
+const myarr = [2,'malmal',false,"goodbye",23];
+const arr_1 = myarr;
+arr_1.pop();
 
-let myPromise = new Promise(function(myResolve, myReject){
-    let req = new XMLHttpRequest();
-    req.open('GET',"mycar.html");
-    req.onload = function() {
-        if (req.status == 200) {
-            myResolve(req.response);
-        }else{
-            myReject("File not Found.");
-        }
-    };
-    req.send();
-});
+console.log("orginel arr before changing the arr_1: ", myarr);
+console.log("arr_1 which refrenced , pop last item :", arr_1);
 
-myPromise.then(
-    function(value) {myDisplayer(value);},
-    function(error) {myDisplayer(error);}
-);
+console.log("-------------------------------------------");
+
+let num = 23;
+let num1 = num;
+num1 = num1 + 7;
+
+console.log("orginel num: ",num);
+console.log("num which refrenced , added 7 :", num1);
